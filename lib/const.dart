de12357d0a213b0a13a04c7f58d0c060dbf5a174
Andrey <rankov@gmail.com>
2022-01-05 21:37:47 +0200

    Main screen (#1)
    * Task 1. Main screen
    
    * Extract App class
    
    * VoidCallback added
    
    * Add primary swatch
    
    * Code cleanup

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+//Strings
+const titleMain = 'Главная';
+const title1 = '4 квадрата';
+const title2 = 'Цветной список';
+const title3 = 'Codelab';
+const title4 = 'Асинхронный запрос';
---
+++ lib/widgets/home_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomeButton extends StatelessWidget {
+  final String text;
+  final VoidCallback? onPressed;
+
+  const HomeButton({required this.text, this.onPressed, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(text),
+          const Icon(
+            Icons.arrow_forward_outlined,
+            size: 20,
+          )
+        ],
+      ),
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
